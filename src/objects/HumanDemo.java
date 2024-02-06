3820bf2cecc8c04586f58a77bef23df4b0d90a7e
teche <techellipticaeducation@gmail.com>
2024-02-06 22:17:28 +0530

    updates

--- src/objects/HumanDemo.java
+++ src/objects/HumanDemo.java
@@ -4,40 +4,80 @@ public class HumanDemo {
 	public static void main(String[] args) {
 		// Instance and static variables
 		
+		// Project Name - British Telecom
+		
+		Human obj = new Human();
+		obj.name = "xyz";
+		Human.country = "Vaibhav";
+	//  for using instance varibale  - object is required
+	//  for using static variable - class ref is required (class variables)
+		
+		
 		Human h1 = new Human();
-		h1.name = "Uma";
-		
-		Human h4 = h1;
-		h4.name= "Vaibhav";
-		
-		h1 = null;
-		h1 = h4;
-		
+//		h1.name = "Tom";
+//		h1.age = 34;
+//		h1.country = "India";
+//		
+//		
+////		Human h4 = h1;
+////		h4.name= "Brad";
+////		
+////		h1 = null;
+////		h1 = h4;
+//		
 		Human h2 = new Human();
-		h2.name = "Gurpreet";
-		
+//		h2.name = "Sachin";
+//		h2.age= 23;
+//		h2.country = "Australia";
+//		
 		Human h3 = new Human();
-		h3.name = "Raman";
+//		h3.name = "Laxman";
+//		h3.country = "Pakistan";
 		
-		h1.speak("Hello How are you guys ?");
-		h2.speak("Hello, I am good");
-		h3.speak("I am fine as well");
-		h4.speak("Hows the weather");
+		
+		System.out.println(h2.country);
+		System.out.println(h1.country);
+		System.out.println(h3.country);
+		
+//		System.out.println(h2.age);
+		
+		
+		
+		
+		
+//		h1.speak("Hello How are you guys ?");
+//		h2.speak("Hello, I am good");
+//		h3.speak("I am fine as well");
+//		h4.speak("Hows the weather");
 	}
 }
 
 class Human{
 	// Chracteristic of Human being
 
+	
+	// instance variables
 	String name;
-	int age;
+	int age= 10;
 	double weight;
 	int height;
-	String country;
+	
+	static String country; // share variable
+	
+	static int counter;
+	
+	static final double PI = 3.14d;
+	// final - value will never change
+	// static - memory will never change
+	public void registerToChat() {
+		counter++;
+	}
 
 	// behaviour
 
 	void speak(String contentToSpeak) {
+		int k = 10; // local variable
+		
 		System.out.println(name +" - " + contentToSpeak);
 	}
 }
---
+++ src/objects/MethodDemo.java
@@ -0,0 +1,61 @@
+package objects;
+
+public class MethodDemo {
+
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		int sum = calc.add(10,20);
+		/*
+				
+				System.out.println(sum);
+				sum = calc.add(10,20);
+				System.out.println(sum);
+		      safasdfas
+		      */
+		calc.m1(1);
+		double d1 = calc.piValue();
+		System.out.println(d1);
+	}
+
+}
+
+
+class Calculator{
+	// input = parameteres
+	// logic = method body
+	// output = return type
+
+	// return keyword - last logical statement of method
+
+	
+	/**
+	 * 
+	 * @param i
+	 * @param j
+	 * @return
+	 */
+	int add(int i, int j){
+		int k = i + j;
+		return k;
+	}
+
+	void m1(int value) {
+		if(value < 10) {
+			System.out.println("value is less then 10");
+			return;
+		}
+		System.out.println("value is more then 10");
+
+	}
+
+	void m2(int value) {
+		System.out.println(value);
+	}	
+	// Non Parameterized method
+	float piValue() {
+		return 3.14f;
+	}
+
+
+
+}
